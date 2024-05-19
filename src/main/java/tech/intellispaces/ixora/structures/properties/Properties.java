c2c96abf94ae5eb5b1c9d9c007abe1ad5d449583
lavor <lavor.andrey@gmail.com>
2024-05-19 14:31:41 +0600

    Properties ontology transitions renamed

--- src/main/java/tech/intellispaces/ixora/structures/properties/Properties.java
+++ src/main/java/tech/intellispaces/ixora/structures/properties/Properties.java
@@ -10,14 +10,14 @@ import tech.intellispacesframework.core.annotation.Transition;
 public interface Properties {
 
   @Transition("4e01aa7a-3fd4-4509-9387-921c30ecf5f9")
-  int asInteger(String path) throws InvalidPropertyException;
+  int getInteger(String path) throws InvalidPropertyException;
 
   @Transition("ffcd77b5-032f-49db-8dfd-c792a19d1005")
-  double asDouble(String path) throws InvalidPropertyException;
+  double getDouble(String path) throws InvalidPropertyException;
 
   @Transition("4cc73514-1a59-4560-ba50-7045360b853c")
-  String asString(String path) throws InvalidPropertyException;
+  String getString(String path) throws InvalidPropertyException;
 
   @Transition("e033ddc5-7f4d-4bbd-8958-8b9c34fdfc95")
-  Properties asProperties(String path) throws InvalidPropertyException;
+  Properties getProperties(String path) throws InvalidPropertyException;
 }
