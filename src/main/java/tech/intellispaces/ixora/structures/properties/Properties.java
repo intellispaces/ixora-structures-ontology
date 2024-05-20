3df475fde5fcb5b9b160e42332e3a30d58285743
lavor <lavor.andrey@gmail.com>
2024-05-20 10:08:03 +0600

    Properties refactoring

--- src/main/java/tech/intellispaces/ixora/structures/properties/Properties.java
+++ src/main/java/tech/intellispaces/ixora/structures/properties/Properties.java
@@ -4,32 +4,32 @@ import tech.intellispacesframework.core.annotation.Domain;
 import tech.intellispacesframework.core.annotation.Transition;
 
 /**
- * Properties are a hierarchical set of configuration properties.
+ * Properties are a hierarchical set of properties.
  */
 @Domain("eccd18a1-ec7a-4949-9acd-2b2fa9576da1")
 public interface Properties {
 
   @Transition("4e01aa7a-3fd4-4509-9387-921c30ecf5f9")
-  int getInteger(String path) throws InvalidPropertyException;
+  int integerValue(String path) throws InvalidPropertyException;
 
   @Transition("ffcd77b5-032f-49db-8dfd-c792a19d1005")
-  double getDouble(String path) throws InvalidPropertyException;
+  double doubleValue(String path) throws InvalidPropertyException;
 
   @Transition("4cc73514-1a59-4560-ba50-7045360b853c")
-  String getString(String path) throws InvalidPropertyException;
+  String stringValue(String path) throws InvalidPropertyException;
 
   @Transition("e033ddc5-7f4d-4bbd-8958-8b9c34fdfc95")
-  Properties getProperties(String path) throws InvalidPropertyException;
+  Properties properties(String path) throws InvalidPropertyException;
 
   @Transition("e8e1fb09-b22e-4e66-bee5-34db85e4ada3")
-  int[] getIntegerList(String path) throws InvalidPropertyException;
+  int[] integerList(String path) throws InvalidPropertyException;
 
   @Transition("101bf792-aa89-4114-ba58-26d78487a4a5")
-  double[] getDoubleList(String path) throws InvalidPropertyException;
+  double[] doubleList(String path) throws InvalidPropertyException;
 
   @Transition("f6d6c228-cfe0-4bcd-80b8-b0bd23e606c7")
-  String[] getStringList(String path) throws InvalidPropertyException;
+  String[] stringList(String path) throws InvalidPropertyException;
 
   @Transition("b48ef2de-6bbc-4c9c-a0b1-e3f1c6ca33d1")
-  Properties[] getPropertiesList(String path) throws InvalidPropertyException;
+  Properties[] propertiesList(String path) throws InvalidPropertyException;
 }
