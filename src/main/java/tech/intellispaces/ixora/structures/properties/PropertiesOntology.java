a639d366ff8ca0f758893cb063eeb9dbdc3e365e
lavor <lavor.andrey@gmail.com>
2024-06-19 10:51:02 +0600

    Rename Properties domain transitions

--- src/main/java/tech/intellispaces/ixora/structures/properties/PropertiesOntology.java
+++ src/main/java/tech/intellispaces/ixora/structures/properties/PropertiesOntology.java
@@ -7,10 +7,10 @@ import tech.intellispaces.framework.core.annotation.Transition;
 public interface PropertiesOntology {
 
   @Transition("1830e924-2c78-4f18-b9eb-39f002cbeea6")
-  Properties stringToProperties(String string) throws InvalidPropertyException;
+  Properties yamlStringToProperties(String string) throws InvalidPropertyException;
 
   @Transition("9bf2dd05-5c47-4bf7-9189-c8000699a8f1")
-  Properties bytesToProperties(byte[] bytes) throws InvalidPropertyException;
+  Properties yamlBytesToProperties(byte[] bytes) throws InvalidPropertyException;
 
   @Transition("cafc0a2e-a1da-44e1-997f-2db62f7e385c")
   <T> T propertiesToData(Properties properties, Class<T> domainClass);
