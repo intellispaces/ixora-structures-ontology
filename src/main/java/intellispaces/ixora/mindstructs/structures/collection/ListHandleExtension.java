0499d718a228967648047a6950bca765cbdd0b24
lavor <lavor.andrey@gmail.com>
2024-07-24 11:51:46 +0600

    Minor change

--- src/main/java/intellispaces/ixora/mindstructs/structures/collection/ListHandleExtension.java
+++ src/main/java/intellispaces/ixora/mindstructs/structures/collection/ListHandleExtension.java
@@ -5,5 +5,5 @@ import tech.intellispaces.framework.core.annotation.Preprocessing;
 @Preprocessing(value = List.class, artifact = "ObjectHandle", extendWith = ListHandleExtension.class)
 public interface ListHandleExtension<E> {
 
-  java.util.List<E> nativeList();
+`  java.util.List<? extends E> nativeList();
 }
